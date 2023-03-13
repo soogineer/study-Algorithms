9ffa4876decb3f5b396d264b183a9f835c2a7146
soogineer <ysooroh54@gmail.com>
2023-03-13 12:59:52 +0900

    praciticing for python

---
+++ programmers/숫자 비교하기.py
@@ -0,0 +1,8 @@
+# https://school.programmers.co.kr/learn/courses/30/lessons/120807
+
+def solution(num1, num2):
+    if num1 == num2:
+        answer = 1
+    else:
+        answer = -1
+    return answer
