35e28dc12e6a4fba09d2be6026af6b64789fb1cc
soogineer <ysooroh54@gmail.com>
2023-03-15 21:30:17 +0900

    praciticing for python

---
+++ programmers/practice.py
@@ -0,0 +1,10 @@
+def twoSum(nums, target):
+  n = len(nums)
+  for i in range(n):
+    for j in range(i+1, n):
+      if nums[i] + nums[j] == target:
+        return True
+        
+    return False
+
+  print(twoSum(nums=[4,1,9,7,5,3,16], target=14))
