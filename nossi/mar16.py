050e4343d48462b136f04396e4c2baba3b0a88d9
soogineer <ysooroh54@gmail.com>
2023-03-16 16:49:53 +0900

    studying nossi with leetcode

---
+++ nossi/mar16.py
@@ -0,0 +1,22 @@
+class ListNode(object):
+    def _init_(self, val=0, next=None, prev=None):
+        self.val = val
+        self.next = next
+        self.prev = prev
+class BrowserHistory(object):
+    def _init(self, homepage):
+        self.head = self.current = ListNode(val=homepage)
+    def visit(self, url):
+        self.current.next = ListNode(val=url, prev=self.current)
+        self.current = self.current.next
+        return None
+    def back(self, steps):
+        while steps > 0 and self.current.prev != None:
+            steps -= 1
+            self.current = self.current.prev
+        return self.current.val
+    def forward(self, steps):
+        while steps > 0 and self.current.next != None:
+            steps -= 1
+            self.current = self.current.next
+            return self.current.val
