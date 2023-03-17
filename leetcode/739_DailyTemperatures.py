5a0bea245f9598a3e375129dcf9565c1707002cd
Yeunsoo Roh <ysooroh54@gmail.com>
2023-03-17 13:52:20 +0900

    Update 739_DailyTemperatures.py

--- leetcode/739_DailyTemperatures.py
+++ leetcode/739_DailyTemperatures.py
@@ -1,6 +1,6 @@
 def dailyTemperatures(temperatures):
     ans = [0] * len(temperatures) # 배열을 0으로 설정
-    stack = [] #stack 선언
+    stack = [] # stack 선언
     for cur_day, cur_temp in enumerate(temperatures):
         while stack and stack[-1][1] < cur_temp:
             prev_day, _ = stack.pop() # _는 prev_temp 값 반환 안 시키게 함
