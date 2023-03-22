8670cb17b6f09877778a1f29a611e83f92875fab
soogineer <ysooroh54@gmail.com>
2023-03-22 19:09:56 +0900

    practice for graph

---
+++ prac-code/bfs.py
@@ -0,0 +1,9 @@
+def bfs(root):
+  visited = []
+  if root is None:
+    return 0;
+  q = deque()
+  q.append(root)
+  while q:
+    cur_node = q.popleft()
+    visited.append(cur_node.value)
---
+++ prac-code/graph-dfs.py
@@ -0,0 +1,17 @@
+graph = {...}
+visited = []
+
+def dfs(cur_v):
+    visited.append(cur_v)
+    for v in graph[cur_v]:
+        if v not in visited:
+            dfs(v)
+
+dfs('A')
+graph = {
+    'A': ['B', 'D', 'E'],
+    'B': ['A', 'C', 'D'],
+    'C': ['B'],
+    'D': ['A', 'B'],
+    'E': ['A']
+}
