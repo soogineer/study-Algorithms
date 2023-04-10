89aded90c808627d07e5387af7759ea0badc7cd1
soogineer <ysooroh54@gmail.com>
2023-04-10 14:36:54 +0900

    prac algorithms

---
+++ thisiscodingtest/배열의원소교체.py
@@ -0,0 +1,22 @@
+n, k = map(int, input().split())
+arr_a = list(map(int, input().split()))
+arr_b = list(map(int, input().split()))
+
+def solution(n, k, arr_a, arr_b):
+    answer = 0
+    arr_a.sort()
+    arr_b.sort(reverse=True)
+
+    for i in range(k):
+        if arr_a[i] < arr_b[i]:
+          arr_a[i], arr_b[i] = arr_b[i], arr_a[i]
+        else:
+          break
+    answer = sum(arr_a)
+    return answer
+
+n = 5
+k = 3
+arr_a = [1, 2, 5, 4, 3]
+arr_b = [5, 5, 6, 6, 5]
+print(solution(n, k, arr_a, arr_b))
