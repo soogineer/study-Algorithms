58462dfa0c07d7822e2b312850b1fdb37631d1b2
soogineer <ysooroh54@gmail.com>
2023-04-11 10:54:48 +0900

    leetcode

---
+++ prac-code/dic-example.py
@@ -0,0 +1,11 @@
+score = {
+    'math': 97,
+    'eng': 89,
+    'kor': 50
+}
+
+
+if 'music' in score:
+    print(score['music'])
+else:
+    score['music'] = 30
--- prac-code/prac-dic.py
+++ prac-code/prac-dic.py
@@ -2,8 +2,4 @@ def two_sum(nums, target):
     memo = {}
     for v in nums:
         memo[v] = True
-    for v in nums:
-      needed_number = target - v
-      if needed_number in memo:
-        return True
-    return False
+    
