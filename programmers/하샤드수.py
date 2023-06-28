a721e5304da2c22fde80cb5c9bae6bda46a5f6bc
soogineer <ysooroh54@gmail.com>
2023-06-28 14:41:28 +0900

    prac algorithm

---
+++ programmers/하샤드수.py
@@ -0,0 +1,10 @@
+# https://school.programmers.co.kr/learn/courses/30/lessons/12947
+
+def solution(x):
+    sum = 0
+    for i in str(x):
+        sum += int(i)
+    if x%sum == 0:
+        return True
+    else:
+        return False
