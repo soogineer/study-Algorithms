76bf16705df18e0fe00b4cd1ed928f26bca1266b
soogineer <ysooroh54@gmail.com>
2023-06-29 14:34:37 +0900

    prac algorithm

---
+++ prac-code/twoSum.py
@@ -0,0 +1,9 @@
+def twoSum(nums, target):
+    n = len(nums)
+    for i in range(n):
+        for j in range(i+1, n): # i와 j가 중복되면 안됨
+            if nums[i] + nums[j] == target:
+                return True
+    return False
+
+print(twoSum(nums=[4,1,9,7,5,3,16], target=14))
