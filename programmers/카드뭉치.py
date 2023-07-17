931ca89ba013049d8e78b0f088be6ace1307b15e
soogineer <ysooroh54@gmail.com>
2023-07-17 21:57:36 +0900

    prac

--- programmers/카드뭉치.py
+++ programmers/카드뭉치.py
@@ -15,3 +15,19 @@ def solution(cards1, cards2, goal):
         
     return 'Yes' if answer == goal else 'No'
     
+
+def solution(cards1, cards2, goal):
+    answer = 'Yes'
+    
+    card1_idx, card2_idx = 0, 0
+    
+    for word in goal:
+        if len(cards1) > card1_idx and word == cards1[card1_idx]:
+            card1_idx += 1
+        elif len(cards2) > card2_idx and word == cards2[card2_idx]:
+            card2_idx += 1
+        else:
+            answer = "No"
+            break
+    
+    return answer
