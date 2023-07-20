fd1952d5b4a5385b5464b601c2084ace7bcd10c4
soogineer <ysooroh54@gmail.com>
2023-07-20 14:09:55 +0900

    prac

--- programmers/피로도.py
+++ programmers/피로도.py
@@ -1,3 +1,4 @@
+#1
 answer = 0
 N = 0
 visited = []
@@ -20,4 +21,25 @@ def solution(k, dungeons):
     N = len(dungeons)
     visited = [0] * N
     dfs(k, 0, dungeons)
+    return answer
+
+
+#2
+answer = 0
+
+def dfs(k, cnt, dungeons, visited):
+    global answer 
+    if cnt > answer:
+        answer = cnt
+    
+    for i in range(len(dungeons)):
+        if not visited[i] and k >= dungeons[i][0]:
+            visited[i] = True
+            dfs(k - dungeons[i][1], cnt + 1, dungeons, visited)
+            visited[i] = False
+        
+def solution(k, dungeons):
+    global answer
+    visited = [False] * len(dungeons)
+    dfs(k, 0, dungeons, visited)
     return answer
